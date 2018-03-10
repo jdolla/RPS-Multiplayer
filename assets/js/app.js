9885775a3e4d0697ff22475518e5d938a929b792
jkososki <jason.kososki@gmail.com>
2018-03-10 07:56:14 -0600

    Initial Commit

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyB8rSUzeYxAG5qcxaHqyP6TZtm6EX30KF0",
+authDomain: "paperrockscissors-kos.firebaseapp.com",
+databaseURL: "https://paperrockscissors-kos.firebaseio.com",
+projectId: "paperrockscissors-kos",
+storageBucket: "paperrockscissors-kos.appspot.com",
+messagingSenderId: "729759339100"
+};
+firebase.initializeApp(config);
