8d30ea3c25ad9c50265016191c232ff3fcf53aae
jkososki <jason.kososki@gmail.com>
2018-03-15 23:25:38 -0500

    last commit :)

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,25 +24,34 @@ function joinGame(player) {
 
             const playerRef = playersRef.push(player);
             game.player = playerRef.toString().substr(playerRef.toString().lastIndexOf("/") + 1);
-     
+
             playerRef.onDisconnect().remove();
 
 
-            game.match = match = db.ref(`match/${game.player}`);
+            game.match = db.ref(`match/${game.player}`);
             game.match.onDisconnect().remove();
-
+            
             document.querySelector("#player>div>div>img").setAttribute("src", player.avatar);
 
+            ready();
             trackPlayers();
+
+            game.chat = db.ref(`chat/${game.player}`);
+            game.chat.onDisconnect().remove();
+
+            db.ref('chat/').on("child_added", function(s){
+               addMessage(s.key, s.val());
+            });
+
 
             const avatarSelection = document.getElementById("avatar-selection");
             const gameRoom = document.getElementById("game-room");
-            window.setTimeout(function(){
+            window.setTimeout(function () {
                 avatarSelection.style.opacity = 0;
-                window.setTimeout(function(){
+                window.setTimeout(function () {
                     avatarSelection.style.display = "none";
                     gameRoom.style.display = "inherit";
-                    window.setTimeout(function(){
+                    window.setTimeout(function () {
                         gameRoom.style.opacity = 1;
                     }, 200)
                 }, 500)
@@ -79,18 +88,17 @@ function connected() {
     return result;
 }
 
-function countDown(){
-    gameInterval = setInterval(function(){
-        let secs = parseInt(time.innerText);  
-        if(secs > 0 ){
+function countDown() {
+    gameInterval = setInterval(function () {
+        let secs = parseInt(time.innerText);
+        if (secs > 0) {
             time.innerText = secs - 1;
         }
         else {
             clearInterval(gameInterval)
             evaluate();
-            countDown();
-            setTimeout(function(){
-                resetMatch();
+            setTimeout(function () {
+                // resetMatch();
             }, 3000);
         }
     }, 1000);
@@ -99,77 +107,98 @@ function countDown(){
 function startGame(opponent) {
     document.querySelector("#opponent img").setAttribute("src", opponent.avatar);
     document.getElementById("opponent-name").innerText = opponent.name;
-    countDown();
 }
 
 function resetMatch() {
+    let res = document.querySelectorAll(".results");
+    for (let i = 0; i < res.length; i++) {
+        res[i].style.display = "none";
+    }
+    
     game.match.remove();
     time.innerText = 10;
 
     mc.style.display = "none";
     op.style.display = "none";
-    
+
     mc.setAttribute("src", "#");
     op.setAttribute("src", "#");
 
     announce.innerText = "";
+
+    ready();
+
 }
 
-function evaluate(){
+function ready() {
+    game.readyPlayer = db.ref(`readyPlayers/${game.player}`);
+    game.readyPlayer.onDisconnect().remove();
+    game.readyPlayer.set("ready");
+
+}
+
+function evaluate() {
     const plays = db.ref("match");
-    plays.once("value", function(s){
+    plays.once("value", function (s) {
         const r = s.toJSON();
         let myChoice = "";
         let opponentChoice = "";
 
-        if(r){
-            if(r.hasOwnProperty(game.opponent)){
+        if (r) {
+            if (r.hasOwnProperty(game.opponent)) {
                 opponentChoice = r[game.opponent].choice;
                 op.setAttribute("src", r[game.opponent].choiceUrl);
                 op.style.display = "inline-block";
             }
 
-            if(r.hasOwnProperty(game.player)){
+            if (r.hasOwnProperty(game.player)) {
                 myChoice = r[game.player].choice;
                 mc.setAttribute("src", r[game.player].choiceUrl);
                 mc.style.display = "inline-block";
             }
         }
 
-       outcome = rps(myChoice, opponentChoice);
+        outcome = rps(myChoice, opponentChoice);
 
-       if(outcome === "tie"){
-           announce.innerText = "It's a Tie!";
-       } else {
-           announce.innerText = `You ${outcome}!!`;
-       }
+        if (outcome === "tie") {
+            announce.innerText = "It's a Tie!";
+        } else {
+            announce.innerText = `You ${outcome}!!`;
+        }
 
     });
+
+    let res = document.querySelectorAll(".results");
+    for (let i = 0; i < res.length; i++) {
+        res[i].style.display = "inherit";
+    }
+
+    game.readyPlayer.remove();
 }
 
-function rps(me, opponent){
-    if(me === opponent){
+function rps(me, opponent) {
+    if (me === opponent) {
         return "tie";
     }
 
-    if(me === ""){
+    if (me === "") {
         return "lose";
     }
 
-    if(opponent === ""){
+    if (opponent === "") {
         return "win";
     }
 
-    if(me === "rock"){
-        if(opponent === "paper"){
+    if (me === "rock") {
+        if (opponent === "paper") {
             return "lose";
-        } else{
+        } else {
             return "win";
         }
     }
-    
-    if(me === "scissors"){
-        if(opponent === "rock"){
+
+    if (me === "scissors") {
+        if (opponent === "rock") {
             return "lose";
         }
         else {
@@ -177,8 +206,8 @@ function rps(me, opponent){
         }
     }
 
-    if(me === "paper"){
-        if(opponent === "scissors"){
+    if (me === "paper") {
+        if (opponent === "scissors") {
             return "lose";
         }
         else {
@@ -201,58 +230,94 @@ function trackPlayers() {
     playersRef.on("value", function (s) {
         const pc = s.numChildren();
         if (pc === 2) {
-            
+
             const allPlayers = s.toJSON();
-            
-            for (let key in allPlayers){
-                if(key != game.player){
+
+            for (let key in allPlayers) {
+                if (key != game.player) {
                     game.opponent = key;
                     break;
                 }
             }
             startGame(allPlayers[game.opponent]);
-            
+
+            db.ref("readyPlayers/").on("value", function (s) {
+                if (s.numChildren() === 2){
+                    countDown();
+                }
+            });
+
+
         } else if (pc < 2 && game.playerCount === 2) {
             playerLost();
         }
         game.playerCount = s.numChildren();
-        
+
     });
     game.playersRef = playersRef;
 }
 
 document.getElementById("join-game").addEventListener("click", function (event) {
-    
+
     const selected = document.querySelector('input[name=avatar]:checked');
     if (!selected) {
         alert("Choose an avatar!");
         return;
     }
-    
+
     const name = document.getElementById("name-input").value;
     if (!name) {
         alert("Enter a name!");
         return;
     }
-    
+
     const avatar = selected.parentNode.getElementsByTagName('img')[0].getAttribute("src");
-    
+
     const player = {
         name: `${name}`,
         avatar: `${avatar}`
     };
-    
+
     connect(player);
 });
 
-function ammoClick(){
+function ammoClick() {
     play.myItem = this.getAttribute("data-item");
     play.myItemUrl = this.getAttribute("src");
     game.match.child("choice").set(`${play.myItem}`);
     game.match.child("choiceUrl").set(`${play.myItemUrl}`);
 }
 
+function addMessage(player, message){
+    
+    newMessage = document.createElement("div");
+    newMessage.innerText = message;
+    
+    if(player == game.player){
+        newMessage.classList.add("player-message");
+    } else {
+        newMessage.classList.add("opponent-message");
+    }
 
+    newMessage.classList.add("message");
+    newMessage.classList.add("last");
+
+    if(messageArea.children.length === 0){
+        newMessage.classList.add("first");
+    }
+
+    if(messageArea.children.length >= 8){
+        messageArea.removeChild(messageArea.firstChild);
+        messageArea.firstChild.classList.add("first");
+    }
+
+    if(messageArea.children.length > 0) {
+        messageArea.lastChild.classList.remove("last");
+    }
+
+    messageArea.appendChild(newMessage);
+
+}
 
 /*************************************************************/
 
@@ -262,7 +327,9 @@ var game = {
     opponent: "",
     playerCount: 0,
     playersRef: null,
-    match: null
+    match: null,
+    readyPlayer: null,
+    chat: null
 }
 
 var play = {
@@ -288,8 +355,25 @@ var gameInterval = null;
 const op = document.getElementById("op-choice");
 const mc = document.getElementById("mc-choice");
 const announce = document.getElementById("announce");
-
+const messageArea = document.getElementById("messages");
 const ammoBtns = document.querySelectorAll(".ammo");
 for (let i = 0; i < ammoBtns.length; i++) {
     ammoBtns[i].addEventListener("click", ammoClick);
 }
+
+
+document.getElementById("restart").addEventListener("click", resetMatch);
+
+const subBtn = document.getElementById("submit-btn");
+subBtn.addEventListener("click", function(e){
+    e.preventDefault();
+
+    cb = document.getElementById("comment-input");
+    if(!cb.value) return;
+    game.chat.set(cb.value);
+    game.chat.remove();
+
+
+    cb.value = "";
+    // commentBox.focus();
+});
